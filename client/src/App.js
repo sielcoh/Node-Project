2490ac80714afdea3b41a3c2af45e249769a1331
sielcoh <sielcoh@gmail.com>
2024-12-17 12:47:17 +0200

    install dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,6 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { UserContextProvider } from './UserContexts';
 import Main from './Main';
-import { useEffect } from 'react';
-import axios from 'axios'
-const URL = 'https://jsonplaceholder.typicode.com/users'
-
-
-// axios.defaults.baseURL = 'http://localhost:4000';
-// axios.defaults.withCredentials = true;
 
 function App() {
   return (
